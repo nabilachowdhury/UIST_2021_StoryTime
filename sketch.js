34735373a4991d470fa8f5bc01f8332ba3e4770e
Nabila Chowdhury <inabila.nsu@gmail.com>
2021-09-27 15:52:53 -0500

    Added green and red light duration for storyTime

--- sketch.js
+++ sketch.js
@@ -16,6 +16,17 @@ document.onkeydown = checkKey;
 
 connectedCube = null
 
+function turnLightOnRGB(r,g,b,duration) {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        connectedCube.turnLightOnRGB(r,g,b,duration);
+        connectedCube.playSE(6);
+        resolve(true)
+      }, duration)
+  })
+}
+
+
 function getToioConnection(){
   connectedCube = P5tCube.connectNewP5tCube();
   console.log(connectedCube);
@@ -32,55 +43,30 @@ async function checkKey(e){
   e = e || window.event;
 
   if(e.keyCode == '67'){
+    //c
     console.log("Waiting for the connection to be established");
     connectedCube = await getToioConnection();
-    connectedCube.turnLightOnRGB(0,255,0,2000);
   }
 
-  if(e.keyCode == '70'){
+  if(e.keyCode == '70'){ //f
     console.log("Waiting for the connection to be established");
     connectedCube2 = await getToioConnection2();
-    connectedCube2.turnLightOnRGB(255,0,0,2000);
+    connectedCube2.turnLightOnRGB(200,0,0,0);
   }
   else if(e.keyCode == '38'){
     //up arrow
     connectedCube.move(8,8,500);
-
-    
-    // P5tCube.connectNewP5tCube().then(
-    //   cube => {
-    //     console.log(cube);
-    //     cube.move(8,8,500); // block to block distance front.
-    //   }
-    // );
   }
   else if(e.keyCode =='40'){
     //down arrow
     connectedCube.move(-8,-8,500);
-    
-
-  }
+    }
   else if(e.keyCode =='37'){
-    //left arrow
-    // P5tCube.connectNewP5tCube().then(
-    //   cube => {
-    //     //cube.rotate(-8,95); //rotate anticlockwise 90
-    //     cube.move(-8,8,95);
-    //   }
-    // );
-
-    connectedCube.move(-8,8,95);
+    connectedCube.move(-8,8,95); // rotate anti-clockwise
   }
   else if(e.keyCode == '39'){
-    //right arrow
-    // P5tCube.connectNewP5tCube().then(
-    //   cube => {
-    //     cube.rotate(8,95); //rotate clockwise 90
-        
-  //     }
-  //   );
-  connectedCube.rotate(8,95);
-
+    
+    connectedCube.rotate(8,95);
   }
   else if (e.keyCode == '87'){
         //w key
@@ -104,18 +90,31 @@ async function checkKey(e){
         var xPos = connectedCube.sensorX;
         var yPos = connectedCube.sensorY;
         connectedCube2.moveTo({
-  x: xPos,
-  y: yPos,
-  angle: -Math.PI / 2,
-  angleType: 0
-}, 80);
+                                x: xPos,
+                                y: yPos,
+                                angle: -Math.PI / 2,
+                                angleType: 0
+                              }, 80);
+  }
+  else if(e.keyCode == '76'){ //L for 15 sec with orange and red warning 3 times
+      await turnLightOnRGB(0,255,0,2500);
+      await turnLightOnRGB(0,255,0,2500);
+      await turnLightOnRGB(0,255,0,2500);
+      await turnLightOnRGB(0,255,0,2500);
+      await turnLightOnRGB(255,100,0,1500);
+      await turnLightOnRGB(255,100,0,1500);
+      await turnLightOnRGB(255,0,0,1500);
+      await turnLightOnRGB(255,0,0,1500);
+
+
   }
 
- /* else if(e.keyCode == '65'){
-    connectedCube.move(10,10,2000);
-    connectedCube.rotate(-8,95);
-    connectedCube.move(8,8,500)
+  else if(e.keyCode =='75'){ //k optional to play sound after each story session
+    // connectedCube2.playSingleNote(100,250);
+    connectedCube2.playSE(6);
+  }
 
-  }*/
   
 }
+
+
