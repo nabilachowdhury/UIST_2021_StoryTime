bd68a3cf1a659a90d105852756977630ff8dd62d
Danika Passler Bates <passlerd@myumanitoba.ca>
2021-10-08 21:44:43 -0500

    updated code

--- sketch.js
+++ sketch.js
@@ -13,6 +13,8 @@ function mouseClicked() {
 }
 
 document.onkeydown = checkKey;
+var player1 = true;
+var story = false;
 
 connectedCube = null
 
@@ -21,6 +23,54 @@ function turnLightOnRGB(r,g,b,duration) {
       setTimeout(() => {
         connectedCube.turnLightOnRGB(r,g,b,duration);
         connectedCube.playSE(6);
+        resolve(true)
+      }, duration)
+  })
+}
+
+function turnLightOnPlayer1(duration) {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        connectedCube.turnLightOnRGB(0,255,0,duration);
+        connectedCube2.turnLightOnRGB(0,255,0,duration);
+        connectedCube3.turnLightOnRGB(255,0,0,duration );
+        connectedCube4.turnLightOnRGB(255,0,0,duration );
+        resolve(true)
+      }, duration)
+  })
+}
+
+function turnLightOnPlayer2(duration) {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        connectedCube3.turnLightOnRGB(0,255,0,duration);
+        connectedCube4.turnLightOnRGB(0,255,0,duration);
+        connectedCube.turnLightOnRGB(255,0,0,duration );
+        connectedCube2.turnLightOnRGB(255,0,0,duration );
+        resolve(true)
+      }, duration)
+  })
+}
+
+function turnLightOnOrangePlayer1(duration) {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        connectedCube.turnLightOnRGB(255,100,0,duration);
+        connectedCube2.turnLightOnRGB(255,100,0, duration);
+        connectedCube3.turnLightOnRGB(255,0,0,duration );
+        connectedCube4.turnLightOnRGB(255,0,0,duration );
+        resolve(true)
+      }, duration)
+  })
+}
+
+function turnLightOnOrangePlayer2(duration) {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        connectedCube.turnLightOnRGB(255,100,0,duration);
+        connectedCube2.turnLightOnRGB(255,100,0, duration);
+        connectedCube3.turnLightOnRGB(255,0,0,duration );
+        connectedCube4.turnLightOnRGB(255,0,0,duration );
         resolve(true)
       }, duration)
   })
@@ -39,51 +89,154 @@ function getToioConnection2(){
   return connectedCube2;
 }
 
+function getToioConnection3(){
+  connectedCube3 = P5tCube.connectNewP5tCube();
+  console.log(connectedCube3);
+  return connectedCube3;
+}
+
+function getToioConnection4(){
+  connectedCube4 = P5tCube.connectNewP5tCube();
+  console.log(connectedCube4);
+  return connectedCube4;
+}
+
+function movePlayer2Toios(){
+  var xPos1 = connectedCube.sensorX;
+  var yPos1 = connectedCube.sensorY;
+  var xPos2 = connectedCube2.sensorX;
+  var yPos2 = connectedCube2.sensorY;
+  connectedCube3.moveTo({
+                                x: xPos1,
+                                y: yPos1,
+                                angle: -Math.PI / 2,
+                                angleType: 0
+                              }, 80);
+  connectedCube4.moveTo({
+                                x: xPos2,
+                                y: yPos2,
+                                angle: -Math.PI / 2,
+                                angleType: 0
+                              }, 80);
+}
+
+function movePlayer1Toios(){
+  var xPos3 = connectedCube3.sensorX;
+  var yPos3 = connectedCube3.sensorY;
+  var xPos4 = connectedCube4.sensorX;
+  var yPos4 = connectedCube2.sensorY;
+  connectedCube.moveTo({
+                                x: xPos3,
+                                y: yPos3,
+                                angle: -Math.PI / 2,
+                                angleType: 0
+                              }, 80);
+  connectedCube2.moveTo({
+                                x: xPos4,
+                                y: yPos4,
+                                angle: -Math.PI / 2,
+                                angleType: 0
+                              }, 80);
+}
+
 async function checkKey(e){
   e = e || window.event;
 
-  if(e.keyCode == '67'){
-    //c
+  if(e.keyCode == '49'){
+    //1
     console.log("Waiting for the connection to be established");
     connectedCube = await getToioConnection();
+    connectedCube.turnLightOnRGB(200,0,0,0);
   }
 
-  if(e.keyCode == '70'){ //f
+  if(e.keyCode == '50'){ //2
     console.log("Waiting for the connection to be established");
     connectedCube2 = await getToioConnection2();
     connectedCube2.turnLightOnRGB(200,0,0,0);
   }
+
+  if(e.keyCode == '51'){ //3
+    console.log("Waiting for the connection to be established");
+    connectedCube3 = await getToioConnection3();
+    connectedCube3.turnLightOnRGB(200,0,0,0);
+  }
+  if(e.keyCode == '52'){ //3
+    console.log("Waiting for the connection to be established");
+    connectedCube4 = await getToioConnection4();
+    connectedCube4.turnLightOnRGB(200,0,0,0);
+  }
+
   else if(e.keyCode == '38'){
     //up arrow
-    connectedCube.move(8,8,500);
+    if(player1){
+      connectedCube.move(8,8,500);
+    }else{
+      connectedCube3.move(8,8,500);
+    }
+    
   }
   else if(e.keyCode =='40'){
     //down arrow
-    connectedCube.move(-8,-8,500);
+    if(player1){
+      connectedCube.move(-8,-8,500);
+    } else {
+      connectedCube3.move(-8,-8,500);
+    }
+    
     }
   else if(e.keyCode =='37'){
-    connectedCube.move(-8,8,95); // rotate anti-clockwise
+    if(player1){
+      connectedCube.move(-8,8,95); // rotate anti-clockwise
+    } else{
+      connectedCube3.move(-8,8,95); // rotate anti-clockwise
+    }
+    
   }
   else if(e.keyCode == '39'){
+    if(player1){
+      connectedCube.rotate(8,95);
+    }else{
+      connectedCube3.rotate(8,95);
+    }
     
-    connectedCube.rotate(8,95);
   }
   else if (e.keyCode == '87'){
         //w key
-        connectedCube2.move(8,8,500);
+        if(player1){
+          connectedCube2.move(8,8,500);
+        }else{
+          connectedCube4.move(8,8,500);
+        }
+        
   }
   else if (e.keyCode == '83'){
         //s key 
-        connectedCube2.move(-8,-8,500);
+        if (player1){
+          connectedCube2.move(-8,-8,500);
+        }else{
+          connectedCube4.move(-8,-8,500);
+        }
   }
   else if (e.keyCode == '65'){
         //a key
-        connectedCube2.move(-8,8,95);
+        if(player1){
+          connectedCube2.move(-8,8,95);
+        } else{
+          connectedCube4.move(-8,8,95);
+        }
   }
   else if (e.keyCode == '68')
   {
         //d key 
-        connectedCube2.rotate(8,95);
+        if(player1){
+          connectedCube2.rotate(8,95);
+        }else{
+          connectedCube4.rotate(8,95);
+        }
+  }
+
+  else if (e.keyCode == '85'){
+    story = false;
   }
 
   else if (e.keyCode == '80'){
@@ -97,16 +250,46 @@ async function checkKey(e){
                               }, 80);
   }
   else if(e.keyCode == '76'){ //L for 15 sec with orange and red warning 3 times
-      await turnLightOnRGB(0,255,0,2500);
-      await turnLightOnRGB(0,255,0,2500);
-      await turnLightOnRGB(0,255,0,2500);
-      await turnLightOnRGB(0,255,0,2500);
+    story = true;
+    while (story){
+      
+      if(i>0){
+        await turnLightOnPlayer1(250);
+        movePlayer1Toios();
+      }
+    
+      player1 = true;
+      await turnLightOnPlayer1(2500);
+      await turnLightOnPlayer1(2500);
+      await turnLightOnPlayer1(2500);
+      await turnLightOnPlayer1(2500);
+      await turnLightOnOrangePlayer1(1500);
+      await turnLightOnOrangePlayer1(1500);
+      
+      await turnLightOnPlayer2(100);
+      movePlayer2Toios();
+      player1 = false;
+      await turnLightOnPlayer2(2500);
+      await turnLightOnPlayer2(2500);
+      await turnLightOnPlayer2(2500);
+      await turnLightOnPlayer2(2500);
+      await turnLightOnOrangePlayer2(1500);
+      await turnLightOnOrangePlayer2(1500);
+    }  
+    
+   
+
+
+  }
+  else if (e.keyCode == '222'){//'
+      await turnLightOnRGB(0,255,0,1500);
+      await turnLightOnRGB(0,255,0,1500);
+      await turnLightOnRGB(0,255,0,1500);
+      await turnLightOnRGB(0,255,0,1500);
       await turnLightOnRGB(255,100,0,1500);
       await turnLightOnRGB(255,100,0,1500);
       await turnLightOnRGB(255,0,0,1500);
       await turnLightOnRGB(255,0,0,1500);
-
-
   }
 
   else if(e.keyCode =='75'){ //k optional to play sound after each story session
